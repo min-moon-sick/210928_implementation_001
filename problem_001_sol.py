54451a690fd81f53ee1d438af27a21954aef219f
min-moon-sick <88085974+min-moon-sick@users.noreply.github.com>
2021-09-28 11:51:26 +0900

    Create problem_001_sol.py

---
+++ problem_001_sol.py
@@ -0,0 +1,21 @@
+x = str(input())
+cnt = 0
+
+x_col = [0, 'a', 'b', 'c', 'd','e','f','g', 'h']
+
+path = [
+  [2, 1],
+  [2, -1],
+  [1, 2],
+  [1, -2]
+]
+# x_col.index, x[1]
+
+for i in range(len(path)):
+  # path[i][0]
+  # path[i][1]
+
+    if 0 < x_col.index(x[0]) + path[i][0] < 9 and 0 < int(x[1]) + path[i][1] < 9 :
+      cnt += 1
+
+print(cnt)
